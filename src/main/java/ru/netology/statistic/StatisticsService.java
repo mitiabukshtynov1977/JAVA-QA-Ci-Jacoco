bb2e8cb72cba4d24ede7133f5f841b4ce170b9c0
Your Name <your_email@example.com>
2025-01-28 18:07:47 +0300

    Обновил Тесты

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,6 +1,8 @@
 package ru.netology.statistic;
 
 public class StatisticsService {
+
+
     public long findMax(long[] incomes) {
         long currentMax = incomes[0];
         for (long income : incomes) {
