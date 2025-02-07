2aebfcd8ec5538ee4507d4b6109c2fb98fcc5e59
Your Name <your_email@example.com>
2025-02-07 13:51:59 +0300

    Дописал тест

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,19 +1,32 @@
 package ru.netology.statistic;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
+
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
-
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual); //метод корректно находит максимальное значение в массиве
     }
+
+
+
+
+
+    @Test
+    void findMaxIfAddIncome() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {2, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12,};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual); // метод корректно находит максимальный доход
+    }
+
 
 }
